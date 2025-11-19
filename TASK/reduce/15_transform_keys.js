13c37e85db9f5eec25b14bd1ac843cd8a1509a7c
hubert.wojcik <hwojcikk@gmail.com>
2025-11-19 21:07:00 +0100

    transform objects

--- TASK/reduce/15_transform_keys.js
+++ TASK/reduce/15_transform_keys.js
@@ -3,7 +3,17 @@
 
 function transformKeys(obj, mapping) {
   // TODO: Uzupełnij implementację używając reduce
-  return Object.entries(obj).reduce(/* ... */);
+  return Object.entries(obj).reduce((acc, curr) => {
+    const [key, value] = curr;
+
+    const mappedKey = mapping[key];
+    if (mappedKey) {
+      acc[mappedKey] = value;
+    } else {
+      acc[key] = value;
+    }
+    return acc;
+  }, {});
 }
 
 // Testy
@@ -13,5 +23,7 @@ const result = transformKeys(obj, mapping);
 const expected = { name: "John", surname: "Doe", age: 30 };
 console.log("Wynik:", result);
 console.log("Oczekiwany:", expected);
-console.log("Test:", JSON.stringify(result) === JSON.stringify(expected) ? "✅ PASS" : "❌ FAIL");
-
+console.log(
+  "Test:",
+  JSON.stringify(result) === JSON.stringify(expected) ? "✅ PASS" : "❌ FAIL"
+);
